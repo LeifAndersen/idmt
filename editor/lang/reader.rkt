ccc9c9aec25f3301ccd929c89d158a7b7318931a
Leif Andersen <leif@leifandersen.net>
2018-04-18 23:44:53 -0400

    pruning`

--- editor/lang/reader.rkt
+++ editor/lang/reader.rkt
@@ -6,11 +6,6 @@ editor
 #:info (λ (key default defproc)
          (case key
            [(color-lexer) lex-editor]
-           [(definitions-text-surrogate) 'editor/lang/surrogate]
-           [(drracket:toolbar-buttons)
-            (define toggle-button
-              (dynamic-require 'editor/lang/surrogate 'toggle-button))
-            (list toggle-button)]
            [else (defproc key default)]))
 
 (require "read-editor.rkt")
