dfc3db966ea393950ab5003283e5c075c0331e5f
Leif Andersen <leif@leifandersen.net>
2018-04-18 23:49:45 -0400

    Even more pruning

--- editor/stdlib.rkt
+++ editor/stdlib.rkt
@@ -3,34 +3,8 @@
 (provide (all-defined-out))
 
 (require "lang.rkt"
-         (for-editor racket/match
-                     racket/set
-                     racket/list
-                     racket/class
-                     racket/serialize
-                     racket/contract/base
-                     racket/string
-                     (except-in racket/gui/base
-                                editor-snip%
-                                editor-canvas%))
-         racket/contract/base
-         file/convertible
-         racket/set
-         racket/list
-         racket/math
-         racket/draw
-         racket/class
-         racket/serialize
-         racket/match
-         racket/string
-         racket/format
-         (for-syntax racket/base
-                     racket/syntax
-                     syntax/parse))
+         (for-syntax racket/base))
 
-;; Because this module is part of the editor language,
-;;  its base lang needs to be something more like racket/base
-;;  rather than editor
 (begin-for-syntax
   (current-editor-base-lang 'racket/base))
 
