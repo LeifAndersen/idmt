f1e2a0a60d8055be99242970f1dc78c4fe5c97cc
Leif Andersen <leif@leifandersen.net>
2018-04-26 14:29:33 -0400

    Use make-*-transformer proc

--- editor/base.rkt
+++ editor/base.rkt
@@ -1,6 +1,8 @@
 (module f racket/base
   (#%plain-module-begin
    (provide (for-editor (all-from-out (submod "stdlib.rkt" editor))))
-   (require "lang.rkt"
-            (for-editor "lang.rkt"
-                        (submod "stdlib.rkt" editor)))))
+   (require "lang.rkt")
+
+   (module+ editor
+     (require "lang.rkt"
+              (submod "stdlib.rkt" editor)))))
--- editor/lang.rkt
+++ editor/lang.rkt
@@ -3,46 +3,18 @@
 (provide (all-defined-out))
 
 (require (for-syntax racket/base
-                     racket/require-transform
                      racket/provide-transform
                      racket/syntax
                      syntax/parse))
 
 ;; ===================================================================================================
 
-(define-for-syntax editor-syntax-introduce (make-syntax-introducer))
-
-;; ===================================================================================================
-
-;; Since the editor submodule is a language detail, we want
-;; a dedicated for-editor require subform.
-(begin-for-syntax
-  (struct for-editor-struct ()
-    #:property prop:require-transformer
-    (λ (str)
-      (λ (stx)
-        (syntax-parse stx
-          [(_ name ...)
-           #:with (marked-name ...) (editor-syntax-introduce #'(name ...))
-           #:with r/b (editor-syntax-introduce (format-id stx "racket/base"))
-           (syntax-local-lift-module-end-declaration
-            #`(module+ editor
-               (require r/b marked-name ...)))])
-        (values '() '())))
-    #:property prop:provide-pre-transformer
-    (λ (str)
-      (λ (stx mode)
-        (syntax-parse stx
-          [(_ name ...)
-           (syntax-local-lift-module-end-declaration
-            #`(module+ editor
-               (provide name ...)))
-           #'(for-editor name ...)])))
-    #:property prop:provide-transformer
-    (λ (str)
-      (λ (stx mode)
-        (syntax-parse stx
-          [(_ name ...)
-           '()])))))
-
-(define-syntax for-editor (for-editor-struct))
+(define-syntax for-editor
+  (make-provide-pre-transformer
+   (λ (stx mode)
+     (syntax-parse stx
+       [(_ name ...)
+        (syntax-local-lift-module-end-declaration
+         #`(module+ editor
+             (provide name ...)))
+        #'(only-meta-in 0)]))))
